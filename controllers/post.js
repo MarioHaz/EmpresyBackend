c9145c90eb564d34649ddf97db89c4d152c31bec
Mario Hazbun <mariohazbun@gmail.com>
2023-09-12 09:01:21 -0500

    cambio get all post

--- controllers/post.js
+++ controllers/post.js
@@ -23,39 +23,12 @@ exports.createPost = async (req, res) => {
 // };
 exports.getAllPosts = async (req, res) => {
   try {
-    const followingTemp = await User.findById(req.user.id).select("following");
-    const following = followingTemp.following;
-    const economicSectorTemp = await User.findById(req.user.id).select(
-      "Economic_Sector"
-    );
-    const userEconomicSector = economicSectorTemp.Economic_Sector;
-    const users = await User.find({ Economic_Sector: userEconomicSector });
-
-    const followingPosts = await Post.find({
-      $or: [
-        { user: { $in: users.map((user) => user.id) } }, // Filter by users with the same economic sector
-        { user: { $in: following } },
-        { user: { $eq: req.user._id } }, // Filter out posts of followed users and the logged-in user
-      ],
-    })
+    const visitorPosts = await Post.find()
       .populate("user", "company_Name picture username cover Economic_Sector")
       .populate("comments.commentBy", "company_Name username picture")
-      .sort({ createdAt: -1 })
-      .limit(10);
-    const followingPostsIds = followingPosts.map((post) => post._id);
-    const remainingPosts = await Post.find({
-      _id: { $nin: followingPostsIds }, // Filter out posts that are already fetched in the 'followingPosts'
-    })
-      .populate("user", "company_Name picture username cover Economic_Sector")
-      .populate("comments.commentBy", "company_Name username picture")
-      .sort({ createdAt: -1 })
-      .limit(10);
+      .sort({ createdAt: -1 });
 
-    const allPosts = followingPosts.concat(remainingPosts).flat();
-    allPosts.sort((a, b) => {
-      return b.createdAt - a.createdAt;
-    });
-    res.json(allPosts);
+    res.json(visitorPosts);
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
